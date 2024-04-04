cf292d3b0f37bd098026eef5516da8f071a28823
Jacobs Agyei <agyeijacobs@outlook.com>
2024-04-04 23:02:11 +0000

    Initial commit

---
+++ src/test/java/com/aries/diningreview/DiningreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aries.diningreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
